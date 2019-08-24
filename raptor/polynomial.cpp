b105acdd7a3b5dc1d4863f9c01529ac2ac5923e8
vishnupetla <vishnupetla@gmail.com>
2019-08-24 13:42:43 +0530

    raptor

---
+++ raptor/polynomial.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? a;
+
+   raptor_prompt_variable_zzyz ="Enter x to get result of x^2+x+2";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> a;
+   cout << (a*a+a+2) << endl;
+   return 0;
+}
